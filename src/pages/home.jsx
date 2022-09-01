6c259827f27050dc5609b6ecd65f3ba4d9e2ad9b
Stephen Loquet <sloquet@gmail.com>
2022-09-01 17:42:24 -0500

    add facial movements

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import "./home.css";
+import { calculateBrow, calculateLeftPupil, calculateRightPupil, calculatePupilHeight } from "./faceFunctions";
 
 
-// move eyes, brows with mouse
 // hair bounce/swish in transform add rotate -5/5 deg alternate
+// must also track while on nav bar
 // add decoration based on month
 
 class Home extends React.Component {
@@ -45,22 +46,13 @@ class Home extends React.Component {
         const yBottom = headRec.top + headRec.height;
         const yTop = headRec.top;
         
-        let lMoveX = this.calculateLeftPupil(xVal, centerWidthLeft, centerWidthTrue, leftEdge);
-        let rMoveX = this.calculateRightPupil(xVal, centerWidthTrue, centerWidthRight, rightEdge);
-        let yMove = this.calculatePupilHeight(yVal, centerHeight, yBottom, yTop);
-        let lBrowRotate = '';
-        let rBrowRotate = '';
-
-        if (yVal < yTop) {
-            yVal = yTop;
-        } else if (yVal> yBottom) {
-            yVal = yBottom
-        }
-        let bMoveIntLeft = -20 + 30 * ((yVal - yTop)/(yBottom-yTop))
-        bMoveIntLeft = Math.floor(bMoveIntLeft);
-        let bMoveIntRight = -1 * bMoveIntLeft;
-        let bMoveLeft = {transform: `rotate(${bMoveIntLeft}deg)`}
-        let bMoveRight = {transform: `rotate(${bMoveIntRight}deg)`}
+        let lMoveX = calculateLeftPupil(xVal, centerWidthLeft, centerWidthTrue, leftEdge);
+        let rMoveX = calculateRightPupil(xVal, centerWidthTrue, centerWidthRight, rightEdge);
+        let yMove = calculatePupilHeight(yVal, centerHeight, yBottom, yTop);
+        let bMoveLeftInt = calculateBrow(yVal, yTop, yBottom);
+        let bMoveRightInt = -1 * bMoveLeftInt;
+        let bMoveLeft = {transform: `rotate(${bMoveLeftInt}deg)`}
+        let bMoveRight = {transform: `rotate(${bMoveRightInt}deg)`}
 
         const lMove = {
             left : lMoveX,
@@ -75,74 +67,6 @@ class Home extends React.Component {
         this.setState({rEyeMove: rMove, lEyeMove: lMove, lBrowMove: bMoveLeft, rBrowMove: bMoveRight});
     }
 
-    calculateLeftPupil(xVal, centerWidthLeft, centerWidthTrue, leftEdge) {
-        let lMoveX = '';
-        if (xVal < leftEdge) {
-            lMoveX = '35%';
-        } else if (xVal > centerWidthTrue) {
-            lMoveX = '65%';
-        } else if (xVal <= centerWidthLeft) {
-            let leftVal = 50 - (15 * ((centerWidthLeft - xVal) / (centerWidthLeft - leftEdge)));
-            leftVal = Math.floor(leftVal);
-            lMoveX = `${leftVal}%`
-        } else if (xVal > centerWidthLeft) {
-            let leftVal = 65 - 15 * ((centerWidthTrue - xVal)/ (centerWidthTrue - centerWidthLeft));
-            leftVal = Math.floor(leftVal);
-            lMoveX = `${leftVal}%`
-        } else {
-            lMoveX = '50%';
-            console.log('lMoveX error')
-        }
-
-        return lMoveX;
-    }
-
-    calculateRightPupil(xVal, centerWidthTrue, centerWidthRight, rightEdge) {
-        let rMoveX = '';
-
-        if (xVal < centerWidthTrue) {
-            rMoveX = '35%';
-        } else if (xVal > rightEdge) {
-            rMoveX = '65%';
-        } else if (xVal <= centerWidthRight) {
-            let rightVal = 50 - (15 * ((centerWidthRight - xVal) / (centerWidthRight - centerWidthTrue)));
-            rightVal = Math.floor(rightVal);
-            rMoveX = `${rightVal}%`
-        } else if (xVal > centerWidthRight) {
-            let rightVal = 50 + 15 * ((xVal - centerWidthRight)/ (rightEdge - centerWidthRight));
-            rightVal = Math.floor(rightVal);
-            rMoveX = `${rightVal}%`
-        } else {
-            rMoveX = '50%'
-            console.log('rMoveX error')
-        }
-
-        return rMoveX;
-    }
-
-    calculatePupilHeight(yVal, centerHeight, yBottom, yTop) {
-        let yMove = '';
-
-        if (yVal < yTop) {
-            yMove = '25%'
-        } else if (yVal > (yBottom)) {
-            yMove = '75%'
-        } else if (yVal < centerHeight) {
-            let yInt = 50 - 25 * ((centerHeight - yVal)/(centerHeight - yTop) )
-            yInt = Math.floor(yInt);
-            yMove = `${yInt}%`;
-        } else if (yVal > centerHeight) {
-            let yInt = 50 + 25 * ((yVal - centerHeight)/(yBottom - centerHeight))
-            yInt = Math.floor(yInt);
-            yMove = `${yInt}%`;
-        }
-
-        return yMove;
-    }
-
-    calculateBrow() {
-
-    }
     
     
     render() {
