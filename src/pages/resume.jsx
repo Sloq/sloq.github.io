61161ad7fd874426480100af3c0a81edcc1289bd
Stephen Loquet <sloquet@gmail.com>
2022-09-28 19:24:43 -0500

    update resume pages

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
+// import { connect } from 'react-redux';
+import { useSelector} from "react-redux";
+import "./resume.css";
 import doc from '../StephenLoquet_Resume.pdf'
+import ResumeMobile from './resumeMobile';
 
-function Resume() {
-    console.log("resume")
+const Resume = () => {
+    const mobile = useSelector(state => state.mobile.mobile);
+
+    if (mobile) {
+        return <ResumeMobile/>;
+    }
+
     return (
-        <div>
-            <a href={doc} download="StephenLoquet_Resume">Download My Resume</a>
+        <div className='resume'>
+            {/* <a className='resume-link' href={doc} download="StephenLoquet_Resume">Download My Resume</a> */}
             <object data={doc} type="application/pdf" width="100%" height="800">
-                {/* <p>Alternative text - include a link <a href="http://sample.pdf">to the PDF!</a></p> */}
             </object>
 
         </div>
     );
 }
-
+  
 export default Resume;
