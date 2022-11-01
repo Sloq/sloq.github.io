3f4ac85ad541c11fa27fdcbd697841215c8e4b43
Stephen Loquet <sloquet@gmail.com>
2022-11-01 16:04:51 -0500

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 ## Personal Website
 Built using React and Redux
+
+Todo:
+Fix 404 for direct routing to routes. Custom 404 redirect to index.html
+
+Deploying to github pages:
+```sh
+
+npm run build
+npm run deploy
+
+```
--- src/layout.jsx
+++ src/layout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
 import NavbarContainer from "./navbar/navbarContainer";
-import NavBarContainer from "./navbar/navbarContainer";
 
 const Layout = () => {
   return (
